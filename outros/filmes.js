316a09da0f930a67a16c94594b9607d1826b95f3
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-04-07 01:50:10 -0300

    Add files via upload

---
+++ outros/filmes.js
@@ -0,0 +1,14 @@
+const filmes = (prefix) => { 
+	return `                 	
+╠══✪〘 FILMES ABAIXO 〙✪══
+║
+╠➥ *${prefix}harrypotter1*
+╠➥ *${prefix}jogadorn1*
+╠➥ *${prefix}em breve*
+╠➥ *${prefix}em breve*
+╠➥ *${prefix}em breve*
+║
+║ *se quiser filme ou anime,série chama meu dono*
+╠══✪〘 *OLAFBOT* 〙✪══`
+}
+exports.filmes = filmes
